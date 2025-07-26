ab31a5d77ceb5b9894eba88435c9b8d800058e35
Adrián García <akrian231@gmail.com>
2025-07-26 18:53:37 +0200

    levantar aplicacion web

--- terraform/vm.tf
+++ terraform/vm.tf
@@ -39,7 +39,7 @@ resource "azurerm_network_security_group" "nsg" {
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = "443"
+    destination_port_range     = "80"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
